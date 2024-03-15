e04feabb37a8e96705c58db857838aef0a9c530f
raymondchiu10 <raymondchiu10@gmail.com>
2024-03-14 17:52:46 -0700

    added analytics

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,40 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import * as path from "path";
+import { VitePluginRadar } from "vite-plugin-radar"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio/",
-  plugins: [
-    react(),
-  ],
-  resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: path.resolve(__dirname, "src")
-      },
-      {
-        // this is required for the SCSS modules
-        find: /^~(.*)$/,
-        replacement: "$1"
-      }
-    ]
-  },
-  server: {
-    watch: {
-      usePolling: true
-    },
-    host: true,
-    strictPort: true,
-    port: 8080
-  }
+	base: "/portfolio/",
+	plugins: [
+		react(),
+		VitePluginRadar({
+			gtm: [
+				{
+					id: "GTM-MFV2S2JJ",
+				}
+			],
+		})
+	],
+	resolve: {
+		alias: [
+			{
+				find: "@",
+				replacement: path.resolve(__dirname, "src")
+			},
+			{
+				// this is required for the SCSS modules
+				find: /^~(.*)$/,
+				replacement: "$1"
+			}
+		]
+	},
+	server: {
+		watch: {
+			usePolling: true
+		},
+		host: true,
+		strictPort: true,
+		port: 8080
+	}
 });
